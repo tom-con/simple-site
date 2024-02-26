ac6d54e62393c2a49a7391cc438e5a81e189a922
tom-con <tconchie@gmail.com>
2024-02-26 15:24:54 +0600

    Adds urlshortener example

--- app/page.js
+++ app/page.js
@@ -1,13 +1,20 @@
-const BASE_URL = "https://www.amdoren.com/api/currency.php"
+const BASE_URL = "https://shrtlnk.dev/api/v2/link"
 const API_KEY = process.env.API_KEY
 
-const fromCurrency = "USD"
-const toCurrency = "EUR"
-const amount = "100"
-
+const urlToShorten = {
+	url: "https://www.t3bhutan.com"
+}
 
 async function getData() {
-	const res = await fetch(BASE_URL + "?api_key=" + API_KEY + "&from=" + fromCurrency + "&to=" + toCurrency + "&amount=" + amount)
+	const res = await fetch(BASE_URL, {
+		method: "POST",
+		headers: {
+			"api-key": API_KEY,
+			Accept: "application/json",
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify(urlToShorten)
+	})
 
 	if (!res.ok) {
 		throw new Error('Failed to fetch data')
@@ -21,7 +28,7 @@ export default async function Home() {
 	console.log(data)
 	return (
 		<div className="text-4xl text-white">
-			{data.amount}
+			{/* {data.amount} */}
 		</div>
 	)
 }
